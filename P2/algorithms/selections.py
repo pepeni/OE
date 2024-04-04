28203aeb857aa5077e674837b5e938e5b7def480
Konrad Ziaja <pepeni1415@gmail.com>
2024-04-04 18:01:19 +0200

    Added new selection method

--- P2/algorithms/selections.py
+++ P2/algorithms/selections.py
@@ -9,7 +9,7 @@ def best_selection(population, fitness_function, num_selected, look_for_max = Fa
 
 # Jak szukamy mamksimum, to zmienić look_for_max na True
 def tournament_selection(population, fitness_function, tournament_size=3, look_for_max=False):
-    # TODO poprawić, żeby losowało wymaganą ilość o ile potrzebne?
+    # TODO poprawić, żeby losowało wymaganą ilość o ile potrzebne? Aktualnie zmniejsza populację o 1/3 (+1 jeśli jest reszta)
     winners = []
     population_copy = list(population)
     while population_copy:
@@ -19,3 +19,28 @@ def tournament_selection(population, fitness_function, tournament_size=3, look_f
         for i in tournament_individuals:
             population_copy.remove(i)
     return winners
+
+
+def roulette_wheel_selection(population, fitness_function, look_for_max=False):
+    # TODO jak na razie funkcja wybiera tylko jednego osobnika
+    if look_for_max == False:
+        fitness_values = [1/fitness_function(individual) for individual in population]
+    else:
+        fitness_values = [fitness_function(individual) for individual in population]
+
+    total_probability = sum(fitness_values)
+    probabilities = [p / total_probability for p in fitness_values]
+
+    roulette_wheel = []
+    cumulative_sum = 0
+    for p in probabilities:
+        cumulative_sum += p
+        roulette_wheel.append(cumulative_sum)
+
+    random_number = random.random()
+
+    for i, segment in enumerate(roulette_wheel):
+        if random_number < segment:
+            return population[i]
+
+    return population[-1]
