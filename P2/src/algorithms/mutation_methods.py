1dc3ea34cd63929d4a8aacda50eb357714b2ae85
Karmel920 <mikolajpe2@gmail.com>
2024-04-07 21:01:24 +0200

    added return types

--- P2/src/algorithms/mutation_methods.py
+++ P2/src/algorithms/mutation_methods.py
@@ -5,14 +5,14 @@ from src.individual import Individual
 
 class MutationMethods:
     @staticmethod
-    def edge_mutation(individual: Individual, probability: float):
+    def edge_mutation(individual: Individual, probability: float) -> None:
         for chromosome in individual.chromosomes:
             random_var = random.random()
             if random_var < probability:
                 chromosome.genes[-1] = 1 if chromosome.genes[-1] == 0 else 0
 
     @staticmethod
-    def single_point_mutation(individual: Individual, probability: float):
+    def single_point_mutation(individual: Individual, probability: float) -> None:
         for chromosome in individual.chromosomes:
             random_var = random.random()
             if random_var < probability:
@@ -20,7 +20,7 @@ class MutationMethods:
                 chromosome.genes[index] = 1 if chromosome.genes[index] == 0 else 0
 
     @staticmethod
-    def double_point_mutation(individual: Individual, probability: float):
+    def double_point_mutation(individual: Individual, probability: float) -> None:
         for chromosome in individual.chromosomes:
             size = individual.get_chromosome_length()
             random_var = random.random()
